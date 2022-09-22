507cd4842ad1b764b2b01545e8e2dfd3c4818090
davimc <davimatosc@hotmail.com>
2022-09-22 17:15:27 -0300

    UserController

---
+++ src/main/java/com/devsuperior/movieflix/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.devsuperior.movieflix.controllers;
+
+import com.devsuperior.movieflix.dto.UserDTO;
+import com.devsuperior.movieflix.services.AuthService;
+import com.devsuperior.movieflix.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthService authService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserDTO> getProfile() {
+        UserDTO dto = new UserDTO(authService.authenticated());
+        System.out.println(dto.getEmail());
+        return ResponseEntity.ok().body(dto);
+    }
+}
